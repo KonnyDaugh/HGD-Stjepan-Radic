d9fca7ded74d532136db706a6f7ae4e02b455078
Konny Daugh <natalydiach@gmail.com>
2026-03-09 12:04:35 +0100

    Made a news-card; built a news page

---
+++ src/components/ui/Cards/NewsCard.jsx
@@ -0,0 +1,21 @@
+import divider from '../../../assets/decorations/divider.webp';
+import './NewsCard.css';
+
+function NewsCard({ title, date, text, image }) {
+  return (
+    <article className="news-card">
+      <img src={image} alt={title} className="news-card__image" />
+
+      <div className="news-card__content">
+        <h3 className="news-card__title">{title}</h3>
+        <div className="news-card__date">
+            <img src={divider} alt="divider" className="divider"/>
+            <p>{date}</p>
+        </div>
+        
+      </div>
+    </article>
+  );
+}
+
+export default NewsCard;
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,3 +1,32 @@
-export default function News() {
-  return <h1>Vijesti</h1>
+import "./News.css";
+import NewsCard from '../components/ui/Cards/NewsCard';
+import img from '../assets/images/news1.jpg';
+
+function News() {
+  return (
+    <section className="news">
+        <div className="container">
+            <div className="container__news">
+                <h1>Vijesti</h1>
+                <div className="news__list">
+                    <NewsCard 
+                        title="Novi koncert orkestra"
+                        date="12.03.2026"
+                        image={img}/>
+                    <NewsCard 
+                        title="Novi koncert orkestra"
+                        date="12.03.2026"
+                        image={img}/>
+                    <NewsCard 
+                        title="Novi koncert orkestra"
+                        date="12.03.2026"
+                        image={img}/>
+                </div>
+                <button>Arhiva vijesti</button>
+            </div>
+        </div>
+    </section>
+  )
 }
+
+export default News;
