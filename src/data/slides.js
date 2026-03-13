9979f69e49674f4de9548b66307938d9f3f6d17e
Konny Daugh <natalydiach@gmail.com>
2026-03-13 15:10:31 +0100

    Added swipe, built a Gallery page

---
+++ src/data/slides.js
@@ -0,0 +1,116 @@
+export const slides = [
+  {
+    id: 1,
+    image: "/gallery/slika-2.webp",
+    title: "Nove fotografije",
+    text: "Selfie orkestra s obilježavanja Oluje 2020. godine u Splitu."
+  },
+  {
+    id: 2,
+    image: "/gallery/slika-3.webp",
+    title: "Nove fotografije",
+    text: "Nastup sa panoramskog autobusa za Veliku Gospu 2020. godine - Žrnovnica i Stobreč."
+  },
+  {
+    id: 3,
+    image: "/gallery/slika-4.webp",
+    title: "Nove fotografije",
+    text: "Obilježavanje Dana antifašističke borbe ispred Galerije umjetnina u Splitu, 22.6.2020."
+  },
+  {
+    id: 4,
+    image: "/gallery/slika-5.webp",
+    title: "Nove fotografije",
+    text: "Božićni koncert u atriju OŠ Žrnovnica - 15.12.2019."
+  },
+  {
+    id: 5,
+    image: "/gallery/slika-6.webp",
+    title: "Nove fotografije",
+    text: "Nastup ispred zgrade HNK-a Split u sklopu tradicionalne akcije A DI SI TI?! udruge MoSt - 14.12.2019."
+  },
+  {
+    id: 6,
+    image: "/gallery/slika-7.webp",
+    title: "Nove fotografije",
+    text: "Nastup za Svetog Nikolu u centru Korešnice 2019. godine."
+  },
+  {
+    id: 7,
+    image: "/gallery/slika-8.webp",
+    title: "Nove fotografije",
+    text: "Adventski koncert na splitskoj Rivi - Zimski Akvarel 2019/2020 godine, 1.12.2019."
+  },
+  {
+    id: 8,
+    image: "/gallery/slika-9.webp",
+    title: "Nove fotografije",
+    text: "Zajednička fotografija sa članovima DVD-a Žrnovnica na blagdan Svih svetih 2019. godine ispred župne crkve UBDM u Žrnovnici."
+  },
+  {
+    id: 9,
+    image: "/gallery/slika-10.webp",
+    title: "Nove fotografije",
+    text: "6. dani tovara na Marjanu - performans Ramba Amadeusa i našeg orkestra, 25.10.2019."
+  },
+  {
+    id: 10,
+    image: "/gallery/slika-11.webp",
+    title: "Nove fotografije",
+    text: "Nastup u centru mjesta za Veliku Gospu 2019. godine."
+  },
+  {
+    id: 11,
+    image: "/gallery/slika-12.webp",
+    title: "Nove fotografije",
+    text: "Olujni come back u centru Žrnovnice, 5.8.2019."
+  },
+  {
+    id: 12,
+    image: "/gallery/slika-13.webp",
+    title: "Stare fotografije",
+    text: "Koncert povodom blagdana Velike Gospe 2001. godine u žrnovačkoj Bašti."
+  },
+  {
+    id: 13,
+    image: "/gallery/slika-14.webp",
+    title: "Stare fotografije",
+    text: "Svečana povorka članova DVD-a i Limena glazba pred Vatrogasnim domom 1997. godine povodom Svetog Florijana"
+  },
+  {
+    id: 14,
+    image: "/gallery/slika-15.webp",
+    title: "Stare fotografije",
+    text: "Nastup ispred Banovine u Splitu 1995. godine."
+  },
+  {
+    id: 15,
+    image: "/gallery/slika-16.webp",
+    title: "Stare fotografije",
+    text: "Žrnovački glazbari na trgu u centru mjesta - naslovnica naše ploče koja je u produkciji tadašnjeg Jugotona izdana 1984. godine."
+  },
+  {
+    id: 16,
+    image: "/gallery/slika-17.webp",
+    title: "Stare fotografije",
+    text: "Žrnovačka limena glazba u Pricviću na svečanosti povodom početka izgradnje nove škole - Marko Marin / zrnovnica.hr"
+  },
+  {
+    id: 17,
+    image: "/gallery/slika-18.webp",
+    title: "Stare fotografije",
+    text: "Žrnovački glazbari u maršu kroz splitske ulice 1971. godine."
+  },
+  {
+    id: 18,
+    image: "/gallery/slika-19.webp",
+    title: "Stare fotografije",
+    text: "Žrnovačka glazba krajem 60-ih godina - cesta prema crkvi, kod skretanja za Aljinoviće - foto arhiva: Ante Betin Beg / zrnovnica.hr"
+  },
+  {
+    id: 19,
+    image: "/gallery/slika-20.webp",
+    title: "Stare fotografije",
+    text: "Fotografija glazbara iz davne 1939. godine objavljena u knjizi autora Ivana Javorčića 'Žrnovnica, od davnina do danas'"
+  },
+];
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,3 +1,53 @@
-export default function Gallery() {
-  return <h1>Galerija</h1>
-}
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from "swiper/modules";
+import { slides } from "../data/slides";
+
+import 'swiper/css';
+import "swiper/css/navigation";
+import './Gallery.css';
+
+function Gallery() {
+  return (
+    <div className="gallery__wrapper">
+        
+        <Swiper
+            modules={[Navigation]}
+            navigation={{
+            prevEl: ".gallery-prev",
+            nextEl: ".gallery-next"
+            }}
+            spaceBetween={30}
+            slidesPerView={1}
+            className="gallery-swiper"
+        >
+            {slides.map((slide) => (
+                <SwiperSlide key={slide.id}>
+                    <div className="container">
+                        <div className="gallery-slide">
+
+                            <img
+                            src={slide.image}
+                            alt={slide.title}
+                            loading="lazy"
+                            />
+
+                            <div className="gallery-caption">
+                            <h3>{slide.title}</h3>
+                            <div className='gallery-caption-wrap'>
+                                <button className="gallery-prev">←</button>
+                                <p>{slide.text}</p>
+                                <button className="gallery-next">→</button>
+                            </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+        
+    </div>
+  );
+};
+
+export default Gallery;
