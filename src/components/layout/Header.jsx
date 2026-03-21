7d2523eabac544c6b64b18dbf0d71173d23de75f
Konny Daugh <natalydiach@gmail.com>
2026-03-21 17:11:21 +0100

    Adapt header for tablet

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,10 @@
-import { NavLink } from "react-router-dom"
-import "./Header.css"
-import logo from "../../assets/logo.png"
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import "./Header.css";
+import logo from "../../assets/logo.png";
 
-export default function Header() {
+function Header() {
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="header">
       <div className="header__inner">
@@ -10,15 +12,24 @@ export default function Header() {
           <img src={logo} alt="logo" />
         </div>
 
-        <nav className="header__nav">
-          <NavLink to="/">Početna</NavLink>
+        <nav className={`header__nav ${isOpen ? "header__nav-open" : ""}`}>
+          <NavLink to="/" onClick={() => setIsOpen(false)}>Početna</NavLink>
           <NavLink to="/o-nama">O nama</NavLink>
           <NavLink to="/vijesti">Vijesti</NavLink>
           <NavLink to="/press">Press</NavLink>
           <NavLink to="/galerija">Galerija</NavLink>
           <NavLink to="/kontakti">Kontakti</NavLink>
         </nav>
+
+        <div className="burger" onClick={() => setIsOpen(!isOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
       </div>
     </header>
   )
-}
+};
+
+export default Header;
