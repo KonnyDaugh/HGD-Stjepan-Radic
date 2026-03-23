fe8058fa2f83b890fc3d696522bc5f3d2d39289e
Konny Daugh <natalydiach@gmail.com>
2026-03-23 11:57:14 +0100

    Adapted Gallery page for tablet

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -8,45 +8,50 @@ import './Gallery.css';
 
 function Gallery() {
   return (
-    <div className="gallery__wrapper">
-        
-        <Swiper
-            modules={[Navigation]}
-            navigation={{
-            prevEl: ".gallery-prev",
-            nextEl: ".gallery-next"
-            }}
-            spaceBetween={30}
-            slidesPerView={1}
-            className="gallery-swiper"
-        >
-            {slides.map((slide) => (
-                <SwiperSlide key={slide.id}>
-                    <div className="container">
-                        <div className="gallery-slide">
+    <section className='gallery'>
+        <div className="container">
+            <div className="gallery__inner">
+                <h1>Galerija</h1>
+                <Swiper
+                    modules={[Navigation]}
+                    navigation={{
+                    prevEl: ".gallery-prev",
+                    nextEl: ".gallery-next"
+                    }}
+                    spaceBetween={30}
+                    slidesPerView={1}
+                    className="gallery-swiper"
+                >
+                    {slides.map((slide) => (
+                        <SwiperSlide key={slide.id}>
+                            
+                            <div className="gallery-slide">
 
-                            <img
-                            src={slide.image}
-                            alt={slide.title}
-                            loading="lazy"
-                            />
+                                <img
+                                src={slide.image}
+                                alt={slide.title}
+                                loading="lazy"
+                                />
 
-                            <div className="gallery-caption">
-                            <h3>{slide.title}</h3>
-                            <div className='gallery-caption-wrap'>
-                                <button className="gallery-prev">←</button>
-                                <p>{slide.text}</p>
-                                <button className="gallery-next">→</button>
+                                <div className="gallery-caption">
+                                    <h3>{slide.title}</h3>
+                                    <div className='gallery-caption-wrap'>
+                                        <p>{slide.text}</p>
+                                    </div>
+                                </div>
+
                             </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-        
-    </div>
+                            
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="gallery__controls">
+                    <button className="gallery-prev">←</button>
+                    <button className="gallery-next">→</button>
+                </div>
+            </div>
+        </div>
+    </section>
   );
 };
 
