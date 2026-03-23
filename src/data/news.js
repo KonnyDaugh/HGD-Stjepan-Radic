3cd826701177ae8ed8b2bc01eb114c4edb5ca62b
Konny Daugh <natalydiach@gmail.com>
2026-03-23 11:17:17 +0100

    Adapted News page

--- src/data/news.js
+++ src/data/news.js
@@ -1,4 +1,5 @@
 import NewsImg1 from '../assets/images/news11-03-26.jpg';
+import NewsImg2 from '../assets/images/news-2.webp';
 
 export const news = [
     {
@@ -8,5 +9,13 @@ export const news = [
         image: NewsImg1,
         text: 'Zahvaljujemo gospodinu Mariu Loliću , predsjedniku M.O. i članovima M.O. Žrnovnica na izuzetno vrijednoj donaciji našoj glazbi. Nova tuba će značajno utjecati na kvalitetu izvedbi našeg orkestra. Vaša podrška ključna je za očuvanje i unapređenje tradicije glazbarstva u Žrnovnici.',
         date: '11.03.2026'
+    },
+    {
+        id: 2,
+        title: 'Vrijedna donacija za našu školu!',
+        slug: 'donacija-za-skolu',
+        image: NewsImg2,
+        text: 'Limena glazba HGD „Stjepan Radić“ Žrnovnica – Gradska glazba Split poklonila je školskoj knjižnici stalke za note (leturine). Ovi stalci imat će višestruku namjenu – služit će za izlaganje knjiga i prospekata u knjižnici, a naravno, na njima će svoje mjesto pronaći i note. Posebno zahvaljujemo učenicima 2. M razreda (smjer meteorološki tehničar) koji su zajedno sa svojim razrednikom organizirali prijevoz leturina iz Žrnovnice do škole. Veliko hvala donatoru HGD „Stjepan Radić“ Žrnovnica – Gradska glazba Split, školskoj knjižničarki Žaklini Antonijević te razredniku 2. M Anti Škobalju, koji je ujedno i svirač u HGD Žrnovnica, na uloženom trudu i suradnji. Zajedništvom i dobrom voljom gradimo bogatije i ljepše školsko okruženje!',
+        date: '12.02.2026'
     }
 ] 
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -7,27 +7,14 @@ function News() {
   return (
     <section className="news">
         <div className="container">
-            <div className="container__news">
-                <h1>Vijesti</h1>
+            <div className="news__inner">
+                <h1 className="news__title" >Vijesti</h1>
                 <div className="news__list">
                     {news.map(item => (
                         <NewsCard key={item.id} news={item} />
                     ))}
-
-                    {/* <NewsCard 
-                        title="Novi koncert orkestra"
-                        date="12.03.2026"
-                        image={img}/>
-                    <NewsCard 
-                        title="Novi koncert orkestra"
-                        date="12.03.2026"
-                        image={img}/>
-                    <NewsCard 
-                        title="Novi koncert orkestra"
-                        date="12.03.2026"
-                        image={img}/> */}
                 </div>
-                <Button variant="primary">Arhiva vijesti</Button>
+                <Button className="button" variant="primary">Arhiva vijesti</Button>
             </div>
         </div>
     </section>
