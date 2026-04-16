dd1c34c9e407d4d359e08f76e6f7b10f9e2d2aa1
Natalia Diachenko <natalydiach@gmail.com>
2026-04-16 16:41:16 +0200

    Corrected the indents on all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <section className="hero">
       <div className="hero__content">
         <h1 className='hero__title'>LIMENA GLAZBA ŽRNOVNICA</h1>
-        <p>Osnovana davne 1911. godine, žrnovska limena glazba predstavlja izuzetno vrijednu kulturnu baštinu šireg splitskog područja.</p>
+        <p className='hero__text'>Osnovana davne 1911. godine, žrnovska limena glazba predstavlja izuzetno vrijednu kulturnu baštinu šireg splitskog područja.</p>
         <Link to="/o-nama" className='button button__primary'>Saznaj više</Link>
       </div>
     </section>
